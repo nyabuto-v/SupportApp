f41fc966a61a211852e5e446725418d198e274e3
nyabuto-v <nyabutovice@gmail.com>
2018-10-23 14:20:07 +0300

    modify settings

--- supportapp2/settings.py
+++ supportapp2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -32,7 +33,7 @@ DATABASES = {
     )
 }
 
-ALLOWED_HOSTS = ['supportapp2.herokuapp.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
